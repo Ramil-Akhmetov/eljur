237d9a99afd04e579ecd8334122ed2e12cfdf460
Ramil Akhmetov <ramil147258369@gmail.com>
2024-05-19 18:45:02 +0700

    update user data

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,11 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'patronymic',
+        'phone',
+        'gender',
+        'birthdate',
         'email',
         'password',
     ];
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'surname' => fake()->lastName(),
+            'patronymic' => fake()->firstName(),
+            'phone' => fake()->phoneNumber(),
+            'gender' => fake()->randomElement(['М', 'Ж']),
+            'birthdate' => fake()->date(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
